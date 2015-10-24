76a8d4281b3f625faa94ead6daacf75d230d136f
Garrett Scholtes <scholtgt@mail.uc.edu>
2015-10-24 15:01:05 -0400

    Implemented blockwise Blelloch scan

--- funcs.cu
+++ funcs.cu
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 // Number of elements to put in the test array
-#define TEST_SIZE 64
+#define TEST_SIZE 16
 #define NUM_BINS 10
 
 ////////////////////////////////////////////////////////////////
@@ -10,7 +10,7 @@
 ////////////////////////////////////////////////////////////////
 
 // Number of threads per block (1-d blocks)
-#define BLOCK_WIDTH 8
+#define BLOCK_WIDTH 4
 // Functions to reduce with
 #define ADD 0
 #define MIN 1
@@ -100,9 +100,57 @@ __global__ void histogramKernel(unsigned int* d_hist,
   }
 }
 
-// This performs and *exclusive* scan
-__global__ void scanKernel() {
+// This performs a partial exclusive scan (blockwise) using Blelloch's method
+__global__ void scanKernel(unsigned int* d_cdf,
+                           unsigned int* d_input,
+                           const size_t array_size)
+{
+  __shared__ unsigned int temp[BLOCK_WIDTH<<1];
+  int bx = blockIdx.x;
+  int tx = threadIdx.x;
+  int index = BLOCK_WIDTH * bx + tx;
+  int offset = 1;
 
+  if(2*index + 1 < array_size) {
+    temp[2*index] = d_input[2*index];
+    temp[2*index + 1] = d_input[2*index + 1];
+  }
+  
+  // Up-sweep
+  for(int powOf2 = (2*BLOCK_WIDTH)>>1; powOf2 > 0; powOf2 >>= 1) {
+    __syncthreads();
+    if(tx < powOf2) {
+      int idx1 = offset*(2*tx + 1) - 1 + 2*BLOCK_WIDTH*bx;
+      int idx2 = offset*(2*tx + 2) - 1 + 2*BLOCK_WIDTH*bx;
+      temp[idx2] += temp[idx1];
+    }
+    offset <<= 1;
+  }
+
+  __syncthreads();
+  ///// The below will need to be remembered for multiple blocks /////
+  if(tx == 0) {
+    temp[2*BLOCK_WIDTH*(bx + 1) - 1] = 0;
+  }
+
+  // Down-sweep
+  for(int powOf2 = 1; powOf2 < 2*BLOCK_WIDTH; powOf2 <<= 1) {
+    offset >>= 1;
+    __syncthreads();
+    if(tx < powOf2) {
+      int idx1 = offset*(2*tx + 1) - 1 + 2*BLOCK_WIDTH*bx;
+      int idx2 = offset*(2*tx + 2) - 1 + 2*BLOCK_WIDTH*bx;
+      unsigned int t = temp[idx1];
+      temp[idx1] = temp[idx2];
+      temp[idx2] += t;
+    }
+  }
+
+  __syncthreads();
+  if(2*index + 1 < array_size) {
+    d_cdf[2*index] = temp[2*index];
+    d_cdf[2*index + 1] = temp[2*index + 1];
+  }
 }
 
 void reduce(float* d_array,
@@ -152,6 +200,10 @@ void scan(unsigned int** d_cdf,
           const size_t array_size)
 {
   cudaMalloc((void**) d_cdf, sizeof(unsigned int) * array_size);
+
+  // Note the divide by 2 (a block can handle array of size 2*BLOCK_WIDTH)
+  size_t numBlocks = (1 + ((array_size - 1) / BLOCK_WIDTH))/2;
+  scanKernel<<<numBlocks, BLOCK_WIDTH>>>(*d_cdf, d_input, array_size);
 }
 
 ////////////////////////////////////////////////////////////////
